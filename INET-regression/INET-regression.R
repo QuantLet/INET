1b78d0e066d58a45dc9ed79ae3d555145e2b0672
Boyuan Ning <73546421+ningboyuan@users.noreply.github.com>
2022-03-13 03:02:58 +0100

    Update INET-regression.R

--- INET-regression/INET-regression.R
+++ INET-regression/INET-regression.R
@@ -9,7 +9,7 @@ lapply(libraries, function(x) if (!(x %in% installed.packages())) {
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
 # read market factor and firm characteristics
-fffactors                 = read.csv("F-F_3Factors.csv", header = TRUE, sep = ",", dec = ".")
+fffactors                 = read.csv("F-F3Factors.csv", header = TRUE, sep = ",", dec = ".")
 rownames(fffactors)       = fffactors[,1]
 fffactors[,1]             = fffactors[,2]+fffactors[,5]
 fffactors                 = fffactors[,c(1,3,4)]
@@ -23,15 +23,16 @@ factorvol                 = as.data.frame(factorvol)
 totalconn                 = read.csv("conn_mw.csv", header = TRUE, sep = ",", dec = ".")
 rownames(totalconn)       = totalconn[,1]
 totalconn                 = totalconn[,-1]
+       
+clustering                = read.csv("cc_mw.csv", header = TRUE, sep = ",", dec = ".")
+clustering                = clustering[,-1]
+rownames(clustering)      = rownames(totalconn)
+       
 for (i in 1:3){
   lm_reg                  = lm(clustering[,i]~ factorvol[,1]+factorvol[,2]+factorvol[,3])
   summary(lm_reg)
 }
 
-
-clustering                = read.csv("cc_mw.csv", header = TRUE, sep = ",", dec = ".")
-clustering                = clustering[,-1]
-rownames(clustering)      = rownames(totalconn)
 for (j in 1:12){
   lm_reg                  = lm(clustering[,j]~ factorvol[,1]+factorvol[,2]+factorvol[,3])
   summary(lm_reg)
